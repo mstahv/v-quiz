e5694cf9bdf71cc5b8c12c752e622898badb0296
Matti Tahvonen <matti@vaadin.com>
2014-09-08 10:34:47 +0300

    Added a small hint for users entering the app

--- src/main/java/org/vquiz/LoginWindow.java
+++ src/main/java/org/vquiz/LoginWindow.java
@@ -1,13 +1,16 @@
 package org.vquiz;
 
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Window;
+import org.vaadin.maddon.button.PrimaryButton;
 import org.vaadin.maddon.fields.MTextField;
 import org.vaadin.maddon.form.AbstractForm;
+import org.vaadin.maddon.label.RichText;
 import org.vaadin.maddon.layouts.MVerticalLayout;
 import org.vquiz.domain.User;
 
@@ -18,11 +21,23 @@ public class LoginWindow extends AbstractForm<User> {
     @Override
     protected Component createContent() {
         return new MVerticalLayout(
+                new RichText().withMarkDown("You need a username to join the "
+                        + "quiz. Username must have at least *4 letters*."),
                 new FormLayout(
                         username
                 ),
                 getToolbar()
-        );
+        ).withSpacing(false);
+    }
+
+    @Override
+    protected Button createSaveButton() {
+        return new PrimaryButton("Login");
+    }
+
+    @Override
+    protected Button createCancelButton() {
+        return new Button("Reset");
     }
 
     public LoginWindow() {
